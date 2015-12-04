2903be75d5378120b4f9911fb81976e3ce0b2489
Volodya Esk <vkhatianovskyi@gmail.com>
2015-12-04 18:33:27 +0200

    Update initial setup

--- tests/other/checking_browser.py
+++ tests/other/checking_browser.py
@@ -1,16 +1,32 @@
 # -*- coding: utf-8 -*-
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
 
 driver = webdriver.Firefox()
 driver.get("http://www.google.com")
+driver.implicitly_wait(15)
+search_field = driver.find_element_by_css_selector("input[name=q]")
+print search_field
 print "Firefox successes"
 driver.quit()
 
 
 driver = webdriver.Chrome()
 driver.get("http://www.google.com")
-print "Chrome successes"
-driver.quit()
+wait = WebDriverWait(driver, 10)
+fLocator = "input[name=q]"
+try:
+    search_field = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, fLocator)))
+    print search_field
+    print "Chrome successes"
+except TimeoutException:
+    print "Element is not found"
+finally:
+    driver.quit()
 
 
